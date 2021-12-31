622435173240e30e48486c64c6dad83c38af9d29
Andre <andre_arias.29@hotmail.com>
2021-12-31 02:39:50 -0300

    Segundo commit: Usando componentes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
+import HelloWorld from './components/HelloWorld'; // "./" indica o diretorio atual
 
 function App() {
+
+  const name = 'André'
+  const newName = name.toUpperCase()
+
+  function sum(a, b)
+  {
+    return a+b
+  }
+
+  const url = 'https://via.placeholder.com/150'
+
   return (
     <div className="App">
-      <h1> Olá React </h1>
-      <p> Meu primeiro App</p>
+      <h2>Alterando o JSX</h2>
+      <p>Olá, {newName}</p>
+      <p>Soma: {sum(1,2)} </p>
+      <img src = {url} alt = "Minha Imagem"/>
+      <HelloWorld/>
     </div>
   );
 }
